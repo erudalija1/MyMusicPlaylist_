e9957d6e80e989e7d18f4e09b25b72509804366d
Ema <emaruda@gmail.com>
2021-11-15 00:50:00 +0100

    Created models Song and Category

---
+++ MyMusicPlaylist/Models/Song.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMusicPlaylist.Models
+{
+    public class Song
+    {
+        [Key]
+        public int SongId { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        public string Performer { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        public string Url { get; set; }
+        public int Review { get; set; }
+
+        [Column(TypeName = "bit")]
+        public bool Favourite { get; set; }
+        public DateTime DateOfInput { get; set; }
+        public DateTime DateOfLastEdit { get; set; }
+        public Category SongCategory { get; set; }
+    }
+}
