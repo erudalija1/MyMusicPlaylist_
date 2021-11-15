b549240188686446850e418d65fec64b037138b2
Ema <emaruda@gmail.com>
2021-11-15 22:36:17 +0100

    Installed three EF Nuget packages and created Contextclass

---
+++ MyMusicPlaylist/Models/MyMusicPlayList.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMusicPlaylist.Models
+{
+    public class MyMusicPlayList:DbContext
+    {
+        public MyMusicPlayList(DbContextOptions<MyMusicPlayList> options):base(options)
+        {
+
+        }
+
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
